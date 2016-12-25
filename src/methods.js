3a002d98d0a755c8ab2eb0bcd9a733b2f91f70a6
RijkvanZanten <rijkvanzanten@me.com>
2016-12-25 21:01:06 +0100

    :sparkles: add preference endpoints

--- src/methods.js
+++ src/methods.js
@@ -2,6 +2,8 @@ const { buildPath, performRequest } = require('./methods/base');
 const { createItem, getItems, getItem, updateItem, deleteItem } = require('./methods/items');
 const { createFile, getFiles, getFile, updateFile } = require('./methods/files');
 const { getTables, getTable, createTable } = require('./methods/tables');
+const { createPrivilege, getGroupPrivilege, getTablePrivilege, updatePrivilege } = require('./methods/privileges');
+const { getPreference, updatePreference } = require('./methods/preferences');
 
 module.exports = {
   buildPath,
@@ -20,5 +22,13 @@ module.exports = {
 
   getTables,
   getTable,
-  createTable
+  createTable,
+
+  createPrivilege,
+  getGroupPrivilege,
+  getTablePrivilege,
+  updatePrivilege,
+
+  getPreference,
+  updatePreference
 };
